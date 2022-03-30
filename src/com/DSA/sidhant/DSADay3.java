ad5d380e9c6e4163a6a922c8c7a2db837135d835
sidkhosla <sidkhosla2@gmail.com>
2022-03-31 00:44:55 +0100

    Day 3 DSA HW question added

--- src/com/DSA/sidhant/DSADay3.java
+++ src/com/DSA/sidhant/DSADay3.java
@@ -3,7 +3,7 @@ package com.DSA.sidhant;
 import java.util.Scanner;
 
 public class DSADay3 {
-//1. You've to check whether a given number is prime or not.
+    //1. You've to check whether a given number is prime or not.
 //2. Take a number "t" as input representing count of input numbers to be tested.
 //3. Take a number "n" as input "t" number of times.
 //4. For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
@@ -62,19 +62,63 @@ public class DSADay3 {
 //2. Take as input "n", the count of fibonacci numbers to print.
 //3. Print first n fibonacci numbers.
 //      ---------------------  Solution
+//        Scanner scn = new Scanner(System.in);
+//
+//        int n = scn.nextInt();
+//
+//        int first = 0 , second = 1;
+//
+//        for(int i = 1 ; i <= n ; i = i + 1){
+//
+//            int third = first + second;
+//            System.out.println(first);
+//
+//            first = second;
+//            second = third;
+//        }
+
+
+//        HW- print all prime till n
+//-------first approach--- prints correct result but descending order
+//        Scanner scn = new Scanner(System.in);
+//        int t = scn.nextInt();
+//        while (t != 0) {
+//            int n = t;
+//
+//            boolean isPrime = true;
+//            for (int div = 2; div * div <= n; div++) {
+//                if (n % div == 0) {
+//                    // factor
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//            if (isPrime) {
+//                System.out.println(n);
+//            } else {
+////                System.out.println("not prime");
+//            }
+//                t--;
+//            }
+//        ------second approach
         Scanner scn = new Scanner(System.in);
 
-        int n = scn.nextInt();
+        int min = scn.nextInt(), max = scn.nextInt();
 
-        int first = 0 , second = 1;
-
-        for(int i = 1 ; i <= n ; i = i + 1){
-
-            int third = first + second;
-            System.out.println(first);
-
-            first = second;
-            second = third;
+        while (min <= max) {
+            if (isPrime(min)){
+                System.out.println(min);
+            }
+            min++;
         }
+    }
+    public static boolean isPrime(int num) {
+        for (int div = 2; div * div <= num; div++) {
+            if (num % div == 0) {
+                // factor
+                return false;
+            }
+        }return true;
+
     }
 }
