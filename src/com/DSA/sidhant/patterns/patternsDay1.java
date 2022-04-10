1cf17682e978f8f3f1ca1d83f80ed0886d0798e5
sidkhosla <sidkhosla2@gmail.com>
2022-04-11 00:13:53 +0100

    Day 1 of patterns yeeeet

--- src/com/DSA/sidhant/patterns/patternsDay1.java
+++ src/com/DSA/sidhant/patterns/patternsDay1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class patternsDay1 {
     public static void main(String[] args) {
+        //-------------------09 april
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //
@@ -13,7 +14,7 @@ public class patternsDay1 {
 //            }
 //            System.out.println();
 //        }
-        //-----------------------
+        //-----------------------09 april
 
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
@@ -26,16 +27,41 @@ public class patternsDay1 {
 //            System.out.println();
 //        }
 
-        //--------------------
+        //--------------------09 april
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for (int r = 1; r <= n; r++) {
+//            int nstars = r;
+//            int nspaces = n - r;
+//            // spaces
+//            for (int i = 1; i <= nspaces; i++) {
+//                System.out.print("\t");
+//            }
+//            // stars
+//            for (int i = 1; i <= nstars; i++) {
+//                System.out.print("*\t");
+//            }
+//            // line change
+//            System.out.println();
+//        }
+        //--------------------09 april time point in video 2:10 for dry run
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         for (int r = 1; r <= n; r++) {
-            int nst = n+1 - r;
-            for(int i= 1; i<=nst;i++){
-                System.out.print("*\t");
+            for (int c = 1; c <= n; c++) {
+                // space or star
+                if (r == c || r + c == n + 1) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
             }
-            System.out.println();
+                // line change
+                System.out.println();
+
+
+            }
         }
     }
-}
