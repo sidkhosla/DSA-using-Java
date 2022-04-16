7f5c4db19affcf4fcbfbc92cc043916a685550c6
sidkhosla <sidkhosla2@gmail.com>
2022-04-16 23:40:01 +0100

    reverse_an_array.java

--- src/com/DSA/sidhant/arrays/reverse_an_array.java
+++ src/com/DSA/sidhant/arrays/reverse_an_array.java
@@ -1,14 +1,47 @@
 package com.DSA.sidhant.arrays;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class reverse_an_array {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+    public static void display(int[] a){
+        StringBuilder sb = new StringBuilder();
 
-        int n = scn.nextInt();
+        for(int val: a){
+            sb.append(val + " ");
+        }
+        System.out.println(sb);
+    }
 
-        int[] arr = new int[n];
+    public static void reverse(int[] a){
+        {
+            int temp;
+            int start = 0, end = a.length-1;
+            while (start < end)
+            {
+                temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
 
     }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        reverse(a);
+        display(a);
+    }
+
 }
