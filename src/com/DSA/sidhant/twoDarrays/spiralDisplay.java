b12272fe63f2d943938711af459be62389c55428
sidkhosla <sidkhosla2@gmail.com>
2022-05-12 00:01:05 +0100

    spiral display question for 2d arrays

---
+++ src/com/DSA/sidhant/twoDarrays/spiralDisplay.java
@@ -0,0 +1,53 @@
+package com.DSA.sidhant.twoDarrays;
+
+import java.util.Scanner;
+
+//https://nados.io/question/spiral-display?zen=true
+public class spiralDisplay {
+    public static void main(String[] args) {
+         // spiral
+         Scanner scn = new Scanner(System.in);
+
+         int nr = scn.nextInt() , nc = scn.nextInt();
+         int mat[][] = new int[nr][nc];
+         for(int i = 0 ; i < nr ; i++){
+             for(int j = 0 ; j < nc ; j++){
+                 mat[i][j] = scn.nextInt();
+             }
+         }
+
+         spiralDisplay(mat);
+
+    }
+    public static void spiralDisplay(int mat[][]) {
+        int rmin = 0, cmin = 0, rmax = mat.length - 1, cmax = mat[0].length - 1;
+        int tne = mat.length * mat[0].length;
+        int count = 0;
+        while (count < tne) {
+            // left wall
+            for (int r = rmin, c = cmin; r <= rmax && count < tne; r++) {
+                System.out.println(mat[r][c]);
+                count++;
+            }
+            // bottom wall
+            for (int r = rmax, c = cmin + 1; c <= cmax && count < tne; c++) {
+                System.out.println(mat[r][c]);
+                count++;
+            }
+            // right wall
+            for (int r = rmax - 1, c = cmax; r >= rmin && count < tne; r--) {
+                System.out.println(mat[r][c]);
+                count++;
+            }
+            // top wall
+            for (int r = rmin, c = cmax - 1; c >= cmin + 1 && count < tne; c--) {
+                System.out.println(mat[r][c]);
+                count++;
+            }
+            rmin++;
+            cmin++;
+            rmax--;
+            cmax--;
+        }
+    }
+}
